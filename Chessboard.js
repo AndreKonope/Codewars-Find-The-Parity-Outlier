72681d56871f8976d3cba4f9b9b524ef6ce071d9
Andre <andrew.konopelko@gmail.com>
2019-03-10 16:50:53 +0100

    Project_Chessboard_from EJS

--- Chessboard.js
+++ Chessboard.js
@@ -3,7 +3,7 @@
 Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
 */
 
-var size = 8;
+let size = 8;
 
 var board = "";
 
